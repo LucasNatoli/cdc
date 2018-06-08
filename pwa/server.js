058b519cafab982d9323b3ae0a577f3ea83bd6d1
Lucas Natoli <lucas.natoli@gmail.com>
2018-06-08 16:32:47 -0300

    agregado login form

--- pwa/server.js
+++ pwa/server.js
@@ -3,10 +3,6 @@ var app = express();
 
 app.use(express.static('web'));
 
-<<<<<<< HEAD
 var server = app.listen(3000, function () {
-=======
-var server = app.listen(8080, function () {
->>>>>>> 06ca1d12173267c1c0d0acb91de1540c54916519
     console.log("server running on port.", server.address().port);
 });
