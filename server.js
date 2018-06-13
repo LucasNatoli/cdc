51b7a9a478b3566696573d7a59853fbc80522e3f
Lucas Natoli <lucas.natoli@gmail.com>
2018-06-13 13:28:31 -0300

    Moviendo el server.js a la raiz del proyecto

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 
-app.use(express.static('web'));
+app.use(express.static('pwa/web'));
 
 var server = app.listen(3000, function () {
     console.log("server running on port.", server.address().port);
