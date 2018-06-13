b8d27d8521fb7660f9dc466b097de71ac45f8c10
Lucas Natoli <lucas.natoli@gmail.com>
2018-06-13 16:10:50 -0300

    primera integracion UI - API - store #4 #7 #9

--- pwa/web/scripts/app.js
+++ pwa/web/scripts/app.js
@@ -1,4 +1,51 @@
 
 (function() {
-  'use strict';
-});
+    'use strict';
+
+    var app = {
+        isLoading: true,
+        spinner: document.querySelector('.loader'),
+        loginRow: document.getElementById('login_row')
+    };
+
+    /*****************************************************************************
+    *
+    * Event listeners for UI elements
+    *
+    ****************************************************************************/
+    document.getElementById('btn_ingresar').addEventListener('click', function() {
+        var data = JSON.stringify({
+            "dni": document.getElementById('login_dni').value,
+            "password": document.getElementById('login_password').value
+        });
+        app.verificarCuenta(data);
+    });
+
+    app.hideloginRow = function () {
+        app.loginRow.setAttribute('hidden', true);
+    };
+
+    app.verificarCuenta = function (data){
+        var url = 'http://localhost:3000/accounts/verificar'
+        var request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+          if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                app.hideloginRow();
+              // var response = JSON.parse(request.response);
+              // var results = response.query.results;
+              // results.key = key;
+              // results.label = label;
+              // results.created = response.query.created;
+              // app.updateForecastCard(results);
+            }
+          } else {
+            // // Return the initial weather forecast since no data is available.
+            // app.updateForecastCard(initialWeatherForecast);
+          }
+        };
+        request.open('POST', url, true);
+        request.setRequestHeader('Content-Type', 'application/json');
+        request.send(data);
+    };
+})();;
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,28 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'cdc2'
+});
+
 var appRouter = function (app) {
   app.post("/accounts/verificar", function(req, res) {
-      var user_id = req.body.dni;
-      var token = req.body.clave;
-      console.log(user_id, token);
+
+      var dni = req.body.dni;
+      var password = req.body.password;
+
+      // connection.connect();
+      var sql = "SELECT COUNT(*) count FROM cuenta WHERE dni=" + dni + " AND clave ='" + password + "'"
+      console.log(sql);
+      connection.query(sql, function (error, results, fields) {
+        if (error) throw error;
+        res.send(sql);    
+      });
+
+      // connection.end();
+
+      console.log(dni, password);
   });
 }
 
